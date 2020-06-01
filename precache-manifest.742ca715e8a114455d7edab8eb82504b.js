91482d0074967ec38fc3555d5d8a8746589a01fe
adorjanszasz <ador.szasz@gmail.com>
2020-06-01 20:24:08 +0200

    Updates

---
+++ precache-manifest.742ca715e8a114455d7edab8eb82504b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "007437de778f15caf457a827ea0939e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13f66388cf759f8e2ad5",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "3f69b1724b7a342e8662",
+    "url": "/static/js/2.2571676e.chunk.js"
+  },
+  {
+    "revision": "cc84c9a6f0104e2f2947b19582169e9e",
+    "url": "/static/js/2.2571676e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13f66388cf759f8e2ad5",
+    "url": "/static/js/main.c6fd8d00.chunk.js"
+  },
+  {
+    "revision": "3c1f6f9f9affaf194651",
+    "url": "/static/js/runtime-main.c7da34b7.js"
+  }
+]);
